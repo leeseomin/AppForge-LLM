689a854022df6c713203576c5885b12102b4f145
leeseomin <arrauof@naver.com>
2026-08-10 16:37:07 +0900

    예시 문구 수정:   - 키보드로 플레이하는 간단한 장애물 피하기 웹게임   - 이미지를 WebP로 변환하고 다운로드하는 웹앱   - yt-dlp로 유튜브 자막과 음원을 추출하는 웹앱

---
+++ appforge/resources/web/assets/swimlanesDiagram-ULZ7WXOC-_74iFq-x.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN-CJEq2hIO.js";import{_ as a}from"./mermaid.core-DVF5zrrZ.js";import"./chunk-5VM5RSS4-CaRRIZ6y.js";import"./chunk-XXDRQBXY-ClujAcSv.js";import"./chunk-KBJHAD2P-Bvls3Hhh.js";import"./chunk-2GRJ4B5K-DaY1apOr.js";import"./MarkdownContent-DUiNErd8.js";import"./index-xMFJ7IsS.js";import"./channel-Ch_XqQef.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
